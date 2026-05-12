abe5b8af42648cd4eeaae919d5ca8f17cf9d0e5c
Malakhassan8 <malakhassan2448@gmail.com>
2026-05-13 02:25:18 +0300

    Add new URL patterns for book borrowing and returning

--- library/urls.py
+++ library/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),  # /signup/ → signup_view
     path('login/', views.login_view, name='login'),     # /login/  → login_view
     path('logout/', views.logout_view, name='logout'),  # /logout/ → logout_view
+    path('books/<int:id>/borrow/', views.borrow_view, name='borrow'), # /books/<id>/borrow/ → borrow_view
+    path('books/<int:id>/return/', views.return_view, name='return'), # /books/<id>/return/ → return_view
+    path('borrowed/', views.borrowed_books_view, name='borrowed_books'),  # /borrowed/ → borrowed_books_view
 ]
