6e4741d14da8a064bc1f661956f62b193a463b4c
nour mohamed <nour13.m1f201924@gmail.com>
2026-05-13 04:56:59 +0300

    Update urls.py

--- library/urls.py
+++ library/urls.py
@@ -1,21 +1,17 @@
-from django.urls import path  # lets us define URL patterns
-from . import views           # imports our views.py from same folder
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup'),  # /signup/ → signup_view
-    path('login/', views.login_view, name='login'),     # /login/  → login_view
-    path('logout/', views.logout_view, name='logout'),  # /logout/ → logout_view
-    path('books/<int:id>/borrow/', views.borrow_view, name='borrow'), # /books/<id>/borrow/ → borrow_view
-    path('books/<int:id>/return/', views.return_view, name='return'), # /books/<id>/return/ → return_view
-    path('borrowed/', views.borrowed_books_view, name='borrowed_books'),  # /borrowed/ → borrowed_books_view
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 
     path('books/', views.book_list_view, name='book_list'),
     path('books/add/', views.add_book_view, name='add_book'),
+    path('books/borrowed/', views.borrowed_books_view, name='borrowed_books'),
     path('books/<int:id>/', views.book_details_view, name='book_details'),
+    path('books/<int:id>/borrow/', views.borrow_view, name='borrow_book'),
+    path('books/<int:id>/return/', views.return_view, name='return_book'),
     path('books/<int:id>/edit/', views.edit_book_view, name='edit_book'),
     path('books/<int:id>/delete/', views.delete_book_view, name='delete_book'),
-
-
-
-    
 ]
