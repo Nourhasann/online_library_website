ee11b11aa78b07c91034b72d7a355478865bbc2a
nour mohamed <nour13.m1f201924@gmail.com>
2026-05-13 03:38:41 +0300

    Update urls.py

--- library/urls.py
+++ library/urls.py
@@ -8,4 +8,14 @@ urlpatterns = [
     path('books/<int:id>/borrow/', views.borrow_view, name='borrow'), # /books/<id>/borrow/ → borrow_view
     path('books/<int:id>/return/', views.return_view, name='return'), # /books/<id>/return/ → return_view
     path('borrowed/', views.borrowed_books_view, name='borrowed_books'),  # /borrowed/ → borrowed_books_view
+
+    path('books/', views.book_list_view, name='book_list'),
+    path('books/add/', views.add_book_view, name='add_book'),
+    path('books/<int:id>/', views.book_details_view, name='book_details'),
+    path('books/<int:id>/edit/', views.edit_book_view, name='edit_book'),
+    path('books/<int:id>/delete/', views.delete_book_view, name='delete_book'),
+
+
+
+    
 ]
